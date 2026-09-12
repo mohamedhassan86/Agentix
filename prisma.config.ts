243088ae9f3a89fd6af5241dcbb878cfc7579279
mohamedhassan86 <47507575+mohamedhassan86@users.noreply.github.com>
2026-09-12 23:08:25 +0400

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -20,6 +20,6 @@ export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
     // Priority: System Env > .env.local > .env
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || process.env.POSTGRES_PRISMA_URL,
   },
 });
